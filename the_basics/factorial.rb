cac513763ec4a76fa64d9cdc560ac31d02ed1a84
Daniel Wu <draywu@gmail.com>
2015-08-20 05:42:59 +0000

    first commit

---
+++ the_basics/factorial.rb
@@ -0,0 +1,8 @@
+fivefac = 5 * 4 * 3 * 2
+sixfac = 6 * fivefac
+sevenfac = 7 * sixfac
+eightfac = 8 * sevenfac
+puts fivefac
+puts sixfac
+puts sevenfac
+puts eightfac
---
+++ the_basics/movies.rb
@@ -0,0 +1,18 @@
+movies = {jaws: 1975,
+          anchorman: 2004,
+          man_of_steel: 2013,
+          a_beautiful_mind: 2001,
+          the_evil_dead: 1981}
+          
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
+moviearray = [movies[:jaws], movies[:anchorman], movies[:man_of_steel], movies[:a_beautiful_mind], movies[:the_evil_dead]]
+puts moviearray[0]
+puts moviearray[1]
+puts moviearray[2]
+puts moviearray[3]
+puts moviearray[4]
