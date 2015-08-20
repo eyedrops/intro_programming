c30168a6615ded59ed88bf3d4b15c254dd675aad
Daniel Wu <draywu@gmail.com>
2015-08-20 06:40:34 +0000

    chapter 2 commit

---
+++ variables/age.rb
@@ -0,0 +1,11 @@
+puts 'how old are you'
+age_s = gets.chomp
+age = age_s.to_i
+puts 'in 10 years you will be'
+puts age + 10
+puts 'in 20 years you will be'
+puts age + 20
+puts 'in 30 years you will be'
+puts age + 30
+puts 'in 40 years you will be'
+puts age + 40
---
+++ variables/name.rb
@@ -0,0 +1,13 @@
+puts "what is your name"
+name = gets.chomp
+puts name + ": this guy fucks"
+
+10.times do
+  puts name
+end
+
+puts 'what is your first name'
+firstname = gets.chomp
+puts 'what is your last name'
+lastname = gets.chomp
+puts firstname + " " + lastname
